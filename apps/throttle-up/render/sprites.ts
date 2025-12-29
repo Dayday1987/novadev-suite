a68b4911eaa05e8a79dbfc8b7897a5c2ddf10ef9
David Munoz <davidlmunoz.2021@gmail.com>
2025-12-28 23:08:30 -0600

    Update sprites.ts

--- apps/throttle-up/render/sprites.ts
+++ apps/throttle-up/render/sprites.ts
@@ -7,3 +7,16 @@ export const SPRITE_PATHS = {
   cone: "/assets/entities/cone.png",
   oil: "/assets/entities/oil.png"
 };
+
+export const SPRITE_PATHS = {
+  bike: "/assets/bike/ninja-h2r.png",
+
+  coin: "/assets/entities/coin.png",
+  cone: "/assets/entities/cone.png",
+  oil: "/assets/entities/oil.png",
+
+  asphalt: "/assets/track/asphalt.png",
+  grass: "/assets/track/grass.png",
+  barriers: "/assets/track/barriers.png",
+  grandstand: "/assets/track/grandstand.png"
+};
