2975c3e44a8bfc6c6a249a7816a9d8de49fb4dee
David Munoz <davidlmunoz.2021@gmail.com>
2025-12-28 21:27:04 -0600

    Update sprites.ts

--- apps/throttle-up/render/sprites.ts
+++ apps/throttle-up/render/sprites.ts
@@ -1,7 +1,9 @@
-export interface Sprite {
-  img: HTMLImageElement;
-  w: number;
-  h: number;
-  anchorX: number;
-  anchorY: number;
-}
+// render/sprites.ts
+
+export const SPRITE_PATHS = {
+  bike: "/assets/bike/ninja-h2r.png",
+
+  coin: "/assets/entities/coin.png",
+  cone: "/assets/entities/cone.png",
+  oil: "/assets/entities/oil.png"
+};
