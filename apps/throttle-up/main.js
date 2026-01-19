b110af92bd940138b3e06982d68a53f7ddb77b9f
David Munoz <davidlmunoz.2021@gmail.com>
2026-01-18 20:34:48 -0600

    Decrease torque value for wheelie mechanics

--- apps/throttle-up/main.js
+++ apps/throttle-up/main.js
@@ -68,7 +68,7 @@ const CONFIG = {
     friction: 0.995,          // Friction multiplier (closer to 1 = less friction)
     
     // Wheelie mechanics
-    torque: 0.0010,           // INCREASED: Rotational force applied during wheelie (was 0.001)
+    torque: 0.0003,           // INCREASED: Rotational force applied during wheelie (was 0.001)
     torqueSpeedMult: 0.0001,  // Speed-dependent torque multiplier
     gravity: 0.003,           // Gravity force pulling bike nose down
     damping: 0.93,            // Angular velocity damping (rotation slowdown)
