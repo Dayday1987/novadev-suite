7dcc532294087187b441062ee4d94dbb8e4514a3
David Munoz <davidlmunoz.2021@gmail.com>
2026-02-17 15:53:41 -0600

    Update ide.services.js
    ES commit

--- apps/novadev-ide/Js/ide.services.js
+++ apps/novadev-ide/Js/ide.services.js
@@ -1,1 +1,13 @@
 /* ide.services.js */
+const STORAGE_KEY = "novadev_ide_project_v1";
+
+let files = {};
+
+export function loadProject() {
+  const data = localStorage.getItem(STORAGE_KEY);
+  if (data) files = JSON.parse(data);
+}
+
+export function saveProject() {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(files));
+}
