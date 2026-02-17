b92a7ed47210b49a3249e58a89c3fce6e466f8cf
David Munoz <davidlmunoz.2021@gmail.com>
2026-02-17 15:52:18 -0600

    Update ide.core.js
    ES commit

--- apps/novadev-ide/Js/ide.core.js
+++ apps/novadev-ide/Js/ide.core.js
@@ -1,1 +1,33 @@
 /* ide.core.js */
+let editor = null;
+
+export async function initEditor() {
+
+  await loadMonaco();
+
+  editor = monaco.editor.create(document.getElementById("editor"), {
+    value: "",
+    language: "javascript",
+    theme: "vs-dark",
+    automaticLayout: true,
+    minimap: { enabled: window.innerWidth > 768 }
+  });
+
+  window.addEventListener("resize", () => {
+    editor.layout();
+  });
+}
+
+function loadMonaco() {
+  return new Promise((resolve, reject) => {
+
+    require.config({
+      paths: { vs: "https://unpkg.com/monaco-editor@0.44.0/min/vs" }
+    });
+
+    require(["vs/editor/editor.main"], () => {
+      resolve();
+    }, reject);
+
+  });
+}
