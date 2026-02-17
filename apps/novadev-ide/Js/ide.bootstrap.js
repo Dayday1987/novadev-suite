686b6dabf5505e27259618e0789287e0c0ba365f
David Munoz <davidlmunoz.2021@gmail.com>
2026-02-17 15:51:19 -0600

    Update ide.bootstrap.js
    ES commit

--- apps/novadev-ide/Js/ide.bootstrap.js
+++ apps/novadev-ide/Js/ide.bootstrap.js
@@ -1,2 +1,21 @@
-/* ide.bootstrap.js */
-  
+import { initEditor } from './ide.core.js';
+import { initPanels } from './ide.panels.js';
+import { loadProject } from './ide.services.js';
+
+export async function bootstrapApp() {
+
+  document.addEventListener("DOMContentLoaded", async () => {
+
+    loadProject();
+    initPanels();
+
+    try {
+      await initEditor();
+    } catch (err) {
+      console.error("Monaco failed to load:", err);
+      alert("Editor failed to load.");
+    }
+
+  });
+
+}
