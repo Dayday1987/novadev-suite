6750c160f1f748ebb610546dc2c8ecd8c09216a5
David Munoz <davidlmunoz.2021@gmail.com>
2026-07-12 05:22:28 -0500

    Fix duplicate SPRITE_PATHS declaration
    Removed duplicate declaration of SPRITE_PATHS.

--- apps/throttle-up/render/sprites.ts
+++ apps/throttle-up/render/sprites.ts
@@ -1,13 +1,4 @@
 // render/sprites.ts
-
-export const SPRITE_PATHS = {
-  bike: "/assets/bike/ninja-h2r.png",
-
-  coin: "/assets/entities/coin.png",
-  cone: "/assets/entities/cone.png",
-  oil: "/assets/entities/oil.png"
-};
-
 export const SPRITE_PATHS = {
   bike: "/assets/bike/ninja-h2r.png",
 
